583239d62e42e88e76ee43fb2108ae024ad8c627
Olivier Louvignes <olivier@mgcrea.io>
2019-07-12 22:08:24 +0200

    feat(update): minor changes

--- test/spec/syslog.spec.ts
+++ test/spec/syslog.spec.ts
@@ -1,6 +1,9 @@
-import {SYSLOG_SCHEMA} from '../../src/adapters/syslog';
+import {SEVERITY_LEVEL_VALUES, SYSLOG_SCHEMA} from '../../src/adapters/syslog';
 
 describe('syslog', () => {
+  it('should properly export a schema', () => {
+    expect(SEVERITY_LEVEL_VALUES).toMatchSnapshot();
+  });
   it('should properly export a schema', () => {
     expect(SYSLOG_SCHEMA).toMatchSnapshot();
   });
