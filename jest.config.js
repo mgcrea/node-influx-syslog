15c15d895f57d92ecda0706e9ee08fe08d6a1500
Olivier Louvignes <olivier@mgcrea.io>
2019-07-12 21:29:52 +0200

    feat(update): minor changes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
   setupFiles: ['<rootDir>/test/setup.ts'],
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.ts']
--- test/__mocks__/influx.ts
+++ test/__mocks__/influx.ts
@@ -1,11 +1,12 @@
 // @NOTE https://jestjs.io/docs/en/manual-mocks.html
+import {dir} from 'console';
 
-const d = (obj: any) => console.dir(obj, {depth: 10, colors: true});
+const d = (obj: any) => dir(obj, {depth: 10, colors: true});
 
 const influx = jest.genMockFromModule('influx');
 
 const __mockedWritePoints = jest.fn().mockImplementation(() => {
-  d('writePoints');
+  d('__mockedWritePoints');
 });
 
 export class InfluxDB {
